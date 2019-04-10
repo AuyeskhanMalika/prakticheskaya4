15baf6be3cc2998c56664da544151e8094b9a86d
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-10 23:41:04 +0600

    Добавьте файлы проекта.

---
+++ praktika4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money money = new Money();
+
+            money.SetCountMoney(10);
+            money.SetParMoney(200);
+            Console.WriteLine("Количество купюр: " + money.GetCountMoney());
+            Console.WriteLine("Номинал купюр: " + money.GetParMoney() + "тг.");
+            Console.WriteLine("Денег у вас: " + money.valueMoney() + "тг.");
+            money.CostProduct(2500);
+            money.CountProduct(500);
+
+            Console.Read();
+        }
+    }
+}
